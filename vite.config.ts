f64320494ccd04a56d55a6629d86f724f5feee5b
katsuya yamaguchi <y.katsuya1116@gmail.com>
2024-11-28 15:03:20 +0900

    feat: vitest導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./tests/vitest-setup.ts"],
+    include: ["**/unit/*.{test,spec}.?(c|m)[jt]s?(x)"],
+    globals: true,
+  },
 })
