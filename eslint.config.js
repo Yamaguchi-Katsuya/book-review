538a91f5f7ce75031550e0a055874746a600dbc7
katsuya yamaguchi <y.katsuya1116@gmail.com>
2024-11-28 16:56:18 +0900

    feat: lint設定改修

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,83 @@
 import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from '@typescript-eslint/eslint-plugin'
-import pluginReact from 'eslint-plugin-react'
+import jsConfig from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import reactPlugin from 'eslint-plugin-react'
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-      parserOptions: {
-        ecmaVersion: 2023,
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
+const commonConfig = {
+  files: ['**/*.{js,jsx,ts,tsx}'],
+  languageOptions: {
+    parserOptions: {
+      ecmaVersion: 2023,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
       },
     },
-    plugins: {
-      react: pluginReact,
-    },
-    rules: {
-      ...pluginJs.configs.recommended.rules,
-      ...pluginReact.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-uses-react': 'off',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+      React: 'readonly',
     },
   },
-]
+  plugins: {
+    react: reactPlugin,
+  },
+  rules: {
+    ...jsConfig.configs.recommended.rules,
+    ...reactPlugin.configs.recommended.rules,
+    'react/react-in-jsx-scope': 'off', // 自動インポートに対応
+    'react/jsx-uses-react': 'off', // React のインポート不要
+    'react/jsx-uses-vars': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect', // 使用しているReactのバージョンを自動検出
+    },
+  },
+}
+
+const tsConfig = {
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      parser: '@typescript-eslint/parser',
+      project: './tsconfig.eslint.json',
+      ecmaVersion: 2023,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+    },
+  },
+  plugins: {
+    '@typescript-eslint': tsPlugin,
+  },
+  rules: {
+    ...tsPlugin.configs.recommended.rules,
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+}
+
+const testConfig = {
+  files: ['**/*.{test,spec}.{ts,tsx}'],
+  languageOptions: {
+    globals: {
+      ...globals.jest,
+    },
+  },
+  plugins: {
+    '@typescript-eslint': tsPlugin,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+}
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [commonConfig, tsConfig, testConfig]
