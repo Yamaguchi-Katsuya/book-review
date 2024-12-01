28bef6978e39f7f2194ee411f1d807af8a3c3f01
katsuya yamaguchi <y.katsuya1116@gmail.com>
2024-12-01 16:08:40 +0900

    feat: Homeコンポーネント追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,45 @@
-import { AuthContext } from '@/providers/AuthProvider';
-import { useContext } from 'react';
-import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
 
 function Home() {
-  const { user } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const [, , removeCookie] = useCookies();
-
-  const handleLogout = () => {
-    removeCookie('token');
-    navigate('/login');
-  };
+  const push = useNavigate();
+  const { userAuth } = useContext(AuthContext);
 
   return (
     <>
-      <h1>Home</h1>
-      <p>{user.name}</p>
-      <img src={user.iconUrl} alt="icon" />
-      <button
-        onClick={handleLogout}
-        className="bg-blue-500 text-white p-2 rounded-md w-full text-2xl font-bold"
-      >
-        ログアウト
-      </button>
+      <div className="flex flex-col items-center justify-center gap-2 md:gap-4 lg:gap-8">
+        <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center">
+          書籍レビュー
+        </h2>
+        <p className="text-lg mt-4 text-center">
+          書籍レビューは、書籍の感想を共有するサービスです。<br />
+          あなたの書籍レビューを共有して、他の人と交流しましょう。
+        </p>
+        {userAuth ? (
+          <button
+            onClick={() => push('/reviews')}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md text-sm md:text-md lg:text-lg hover:bg-white hover:text-blue-500 hover:border hover:border-blue-500 transition-colors duration-300"
+          >
+            みんなのレビューを見に行く
+          </button>
+        ) : (
+          <section className="mt-8 flex gap-4">
+            <button
+              onClick={() => push('/login')}
+              className="bg-blue-500 text-white px-4 py-2 rounded-md text-sm md:text-md lg:text-lg hover:bg-white hover:text-blue-500 hover:border hover:border-blue-500 transition-colors duration-300"
+            >
+              ログインする
+            </button>
+            <button
+              onClick={() => push('/signup')}
+              className="bg-blue-500 text-white px-4 py-2 rounded-md text-sm md:text-md lg:text-lg hover:bg-white hover:text-blue-500 hover:border hover:border-blue-500 transition-colors duration-300"
+            >
+              サインアップする
+            </button>
+          </section>
+        )}
+      </div>
     </>
   );
 }
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -6,6 +6,7 @@ import { AuthProvider, AuthContext } from '@/providers/AuthProvider';
 import { useContext } from 'react';
 import BaseLayout from '@/components/BaseLayout';
 import BookPublicReviewList from '@/pages/bookReview/BookPublicReviewList';
+import Home from '@/pages/Home';
 
 const AppRoutes = () => {
   const { userAuth } = useContext(AuthContext);
@@ -13,13 +14,13 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<BaseLayout />}>
+        <Route path="/" element={<Home />} />
         {userAuth ? (
           <>
-            <Route path="/" element={<BookPublicReviewList />} />
-            <Route path="/book-review" element={<BookPublicReviewList />} />
+            <Route path="/reviews" element={<BookPublicReviewList />} />
             {/* ログインしている場合はログインページやサインアップページにアクセスできないようにする */}
-            <Route path="/login" element={<Navigate to="/" replace />} />
-            <Route path="/signup" element={<Navigate to="/" replace />} />
+            <Route path="/login" element={<Navigate to="/reviews" replace />} />
+            <Route path="/signup" element={<Navigate to="/reviews" replace />} />
             <Route path="*" element={<NotFound />} />
           </>
         ) : (
