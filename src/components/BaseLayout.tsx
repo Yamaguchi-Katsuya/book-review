46c6462bae50cd68fd659098439df92d5b87f705
katsuya yamaguchi <y.katsuya1116@gmail.com>
2024-12-01 15:45:59 +0900

    feat: レイアウトコンポーネント追加

---
+++ src/components/BaseLayout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function BaseLayout() {
+  return (
+    <>
+      <main className="min-h-screen flex flex-col">
+        <Header />
+        <Outlet />
+        <Footer />
+      </main>
+    </>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <footer className="flex justify-center items-center bg-blue-500 text-white text-xl md:text-2xl font-bold p-2 md:p-3">
+        <p>&copy; 2024 Book Review</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
